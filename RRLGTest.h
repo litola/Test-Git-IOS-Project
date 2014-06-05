1bbf048ab81bbed8f53900bc1e99c125dc0cde8b
Rafael Lara <rafaellara@rafael.gateway.2wire.net>
2014-06-05 18:10:33 -0500

    added a test class

---
+++ RRLGTest.h
@@ -0,0 +1,13 @@
+//
+//  RRLGTest.h
+//  Git test
+//
+//  Created by Rafael Lara on 05/06/14.
+//  Copyright (c) 2014 Rafael Lara. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RRLGTest : NSObject
+
+@end
---
+++ RRLGTest.m
@@ -0,0 +1,13 @@
+//
+//  RRLGTest.m
+//  Git test
+//
+//  Created by Rafael Lara on 05/06/14.
+//  Copyright (c) 2014 Rafael Lara. All rights reserved.
+//
+
+#import "RRLGTest.h"
+
+@implementation RRLGTest
+
+@end
